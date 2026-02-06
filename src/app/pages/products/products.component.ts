f63d55590e89f10e12f8e88d34c67228a463a66d
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-06 16:52:27 +0530

    Add sidebar and search bar components; include Font Awesome scripts and styles

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../../components/side-bar/side-bar.component";
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [SideBarComponent, SearchBarComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
