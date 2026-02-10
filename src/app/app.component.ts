d57ad6172abfba77d3fc2df2c984c8ddefbfff03
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-10 10:25:32 +0530

    Remove Font Awesome script reference from angular.json and clean up imports in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ProductsComponent } from "./pages/products/products.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent],
+  imports: [ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
