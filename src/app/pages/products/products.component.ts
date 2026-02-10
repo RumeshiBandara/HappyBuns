54a205477b92ecf49da6b718b319c07a8e4af829
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-10 11:06:36 +0530

    Implement ProductCard and ProductCardHolder components in the products page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { ProductCardHolderComponent } from '../../components/product-card-holder/product-card-holder.component';
 
 @Component({
   selector: 'app-products',
-  imports: [SideBarComponent, SearchBarComponent],
+  imports: [SideBarComponent, SearchBarComponent,ProductCardHolderComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
