6cb903505c060da61db7ce94cbe86d9689e92310
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-10 14:13:05 +0530

    Add side-bar component with order details, order card holder, subtotal, total, and place order components

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { OrderCardHolderComponent } from '../order-card-holder/order-card-holder.component';
+import { OrderDetailsComponent } from '../order-details/order-details.component';
+import { SubtotalComponent } from '../subtotal/subtotal.component';
+import { TotalComponent } from '../total/total.component';
+import { PlaceOrderComponent } from "../place-order/place-order.component";
 
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [OrderCardHolderComponent, OrderDetailsComponent, SubtotalComponent, TotalComponent, PlaceOrderComponent],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
