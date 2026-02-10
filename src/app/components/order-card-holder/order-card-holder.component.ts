d5d8941da2d5bb057d6ae93dec0636f01d112833
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-10 13:45:18 +0530

    Add OrderCard component to OrderCardHolder with updated imports and template

--- src/app/components/order-card-holder/order-card-holder.component.ts
+++ src/app/components/order-card-holder/order-card-holder.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { OrderCardComponent } from '../order-card/order-card.component';
 
 @Component({
   selector: 'app-order-card-holder',
-  imports: [],
+  imports: [OrderCardComponent],
   templateUrl: './order-card-holder.component.html',
   styleUrl: './order-card-holder.component.css'
 })
