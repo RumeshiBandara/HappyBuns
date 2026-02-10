39e602ebf4b55b980a1f1d50e2debe195a1c450b
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-10 11:21:43 +0530

    Refactor category section and sidebar components; implement OrderDetails component with template, styles, and tests

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,10 +3,11 @@ import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
 import { ProductCardHolderComponent } from '../../components/product-card-holder/product-card-holder.component';
+import { CategorySectionComponent } from "../../components/category-section/category-section.component";
 
 @Component({
   selector: 'app-products',
-  imports: [SideBarComponent, SearchBarComponent,ProductCardHolderComponent],
+  imports: [SearchBarComponent, ProductCardHolderComponent, CategorySectionComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
