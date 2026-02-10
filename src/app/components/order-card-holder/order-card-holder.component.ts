3f301b89fbd327d21e8bfba0ffe3b49f952339ff
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-10 11:39:26 +0530

    Add OrderCardHolder component with template, styles, and unit tests

---
+++ src/app/components/order-card-holder/order-card-holder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-card-holder',
+  imports: [],
+  templateUrl: './order-card-holder.component.html',
+  styleUrl: './order-card-holder.component.css'
+})
+export class OrderCardHolderComponent {
+
+}
